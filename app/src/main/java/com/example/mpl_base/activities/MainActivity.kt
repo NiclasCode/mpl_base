9dd4890e2e10773e208f259303ade47fe840d63c
Niclas <nicmarcgro@gmail.com>
2023-12-12 15:28:30 +0100

    Demo1

--- app/src/main/java/com/example/mpl_base/activities/MainActivity.kt
+++ app/src/main/java/com/example/mpl_base/activities/MainActivity.kt
@@ -1,29 +1,31 @@
 package com.example.mpl_base.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import com.example.mpl_base.R
 import com.example.mpl_base.util.CalcUtil
+import com.example.mpl_base.util.IS_PRIME
+import com.example.mpl_base.util.NotificationUtil
+import com.example.mpl_base.util.RANDOM_NUMBER
 
-class MainActivity : AppCompatActivity()
-{
+class MainActivity : AppCompatActivity() {
     private lateinit var randomNumberTv: TextView
     private lateinit var randomizeBtn: Button
     private lateinit var notifyBtn: Button
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //         NotificationUtil.createNotificationChannel(this)
+        NotificationUtil.createNotificationChannel(this)
 
         setUI()
     }
 
-    private fun setUI(){
+    private fun setUI() {
         randomNumberTv = findViewById(R.id.main_random_number)
         randomizeBtn = findViewById(R.id.main_btn_randomize)
         notifyBtn = findViewById(R.id.main_btn_notify)
@@ -34,12 +36,33 @@ class MainActivity : AppCompatActivity()
             updateRandomNumber()
         }
 
-        notifyBtn.setOnClickListener{
-//            NotificationUtil.sendNotification(this, title, text, icon, i)
+        notifyBtn.setOnClickListener {
+            val number = randomNumberTv.text.toString().toInt()
+            val isPrime = CalcUtil.checkIfPrime(number)
+
+            val title: String
+            val text: String
+            val icon: Int
+
+            if(isPrime){
+                title = getString(R.string.yay)
+                text = String.format(getString(R.string.answer_text), number, getString(R.string.is_text))
+                icon = R.drawable.icon_true
+            } else {
+                title = getString(R.string.nay)
+                text = String.format(getString(R.string.answer_text), number, getString(R.string.is_not_text))
+                icon = R.drawable.icon_false
+            }
+
+            val notifyIntent = Intent(this, MainActivity::class.java)
+            notifyIntent.putExtra(RANDOM_NUMBER, number)
+            notifyIntent.putExtra(IS_PRIME, isPrime)
+
+            NotificationUtil.sendNotification(this, title, text, icon, notifyIntent)
         }
     }
 
-    private fun updateRandomNumber(){
+    private fun updateRandomNumber() {
         val randomNumber = CalcUtil.rng()
         randomNumberTv.text = randomNumber.toString()
     }
